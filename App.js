e6bdee3529390a11b61e2830028e432bdfad05a2
Younes Henni <henni1989younes@gmail.com>
2019-07-29 11:16:47 +0100

    feat: add linting to project

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import SignInScreen from './src/components/SignInScreen';
 
 export default function App() {
-  return (
-    <SignInScreen />
-  );
+  return <SignInScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
