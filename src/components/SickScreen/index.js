23802a4ddd4819fa3605ba2cb11cbc85528c85d7
Younes Henni <henni1989younes@gmail.com>
2019-08-23 14:16:19 +0100

    feat: add screen elements to sick screen

--- src/components/SickScreen/index.js
+++ src/components/SickScreen/index.js
@@ -1,63 +1,62 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
-// import {
-//   ParagraphView,
-//   StatusView,
-//   IconView,
-//   ResultsText,
-//   MessageOneText,
-//   MessageTwoText,
-//   ItemView,
-//   ItemTextView,
-//   ItemText,
-//   ItemMessage,
-//   FaceHeight,
-//   FaceWidth,
-// } from './styledComponents';
+import {
+  ParagraphView,
+  StatusView,
+  IconView,
+  ResultsText,
+  MessageOneText,
+  ItemView,
+  ItemTextView,
+  ItemText,
+  ItemMessage,
+  FaceHeight,
+  FaceWidth,
+} from '../HealthyScreen/styledComponents';
 
 import { ImageContainer } from '../../common/styledComponents';
-// import { ShadowView, HealthyColors } from '../HomeScreen/styledComponents';
+import { ShadowView, SickColors } from '../HomeScreen/styledComponents';
 
-// import SadFace from '../../common/Icons/Sad.svg';
-// import Yoga from '../../common/Icons/Yoga.svg';
-// import Sports from '../../common/Icons/Sports.svg';
+import SadFace from '../../common/Icons/Sad.svg';
+import Body from '../../common/Icons/Body.svg';
+import Mind from '../../common/Icons/Mind.svg';
 
 const background = require('../../../assets/SickScreen.png');
 
 const SickScreen = () => (
   <ImageContainer source={background}>
     <StatusBar barStyle="light-content" />
-    {/* <SafeAreaView>
+    <SafeAreaView>
       <StatusView>
         <ShadowView>
-          <IconView colors={HealthyColors}>
-            <HappyFace width={FaceWidth} height={FaceHeight} />
+          <IconView colors={SickColors}>
+            <SadFace width={FaceWidth} height={FaceHeight} />
           </IconView>
         </ShadowView>
       </StatusView>
       <ParagraphView>
-        <ResultsText>Medi I am well</ResultsText>
-        <MessageOneText>Say hello to wellness</MessageOneText>
-        <MessageTwoText>A healthy you is a happy you</MessageTwoText>
+        <ResultsText>We got you buddy</ResultsText>
+        {/* <MessageOneText>We got you buddy</MessageOneText> */}
+        <MessageOneText>Is the pain in your</MessageOneText>
       </ParagraphView>
       <ScrollView>
         <ItemView>
-          <Yoga />
+          <Mind />
           <ItemTextView>
-            <ItemText>Yoga</ItemText>
+            <ItemText>Mind</ItemText>
             <ItemMessage>Start your practice here</ItemMessage>
           </ItemTextView>
         </ItemView>
         <ItemView>
-          <Sports />
+          <Body />
           <ItemTextView>
-            <ItemText>Exercise</ItemText>
+            <ItemText>Body</ItemText>
             <ItemMessage>Enroll in classes here</ItemMessage>
           </ItemTextView>
         </ItemView>
       </ScrollView>
-    </SafeAreaView> */}
+    </SafeAreaView>
   </ImageContainer>
 );
 export default SickScreen;
